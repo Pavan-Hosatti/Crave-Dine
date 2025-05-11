45814f3750acdd3a5a002fc033741592f73757ec
Pavan-Hosatti <pavu586@gmail.com>
2025-05-11 14:28:11 +0530

    Delete backend directory

--- backend/routes/reservationRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { sendReservation } = require('../controller/reservation');
-
-const router = express.Router();
-router.post('/send', sendReservation);
-
-module.exports = router;
